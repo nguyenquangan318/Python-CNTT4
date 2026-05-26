b1d9d3dbf6dcbb8bd82c6c267d11668496b6ac3a
An <nguyenquangan318@gmail.com>
2026-05-26 08:38:20 +0700

    Session 07

---
+++ Session 07/demo.py
@@ -0,0 +1,21 @@
+full_name = input("Nhập vào họ và tên")
+# full_name[0] = 'N'
+# full_name = full_name.upper()
+if(full_name.istitle()):
+    print(full_name)
+else:
+    print('Tên không hợp lệ')
+# print(full_name[-5])
+
+# date_of_birth = '25/05/2026'
+# string_name[start:stop:step]
+# date = date_of_birth[:2]
+# month = date_of_birth[3:5]
+# year = date_of_birth[6:10]
+# test_year = date_of_birth[-4:]
+# reverse = date_of_birth[::-1]
+# print(date)
+# print(month)
+# print(year)
+# print(test_year)
+# print(reverse)
